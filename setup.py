f43dcdfedc7e476db23a64c0e3c448e22e111444
Erik Wasser <fuzz@namm.de>
2020-10-20 19:43:47 +0200

    fixed: `find_packages()` call to find the correct directory

--- setup.py
+++ setup.py
@@ -22,8 +22,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=find_packages(where='src', exclude=("tests",)),
-    package_dir={'':'src'},
+    packages=find_packages(where='Tree', exclude=("tests",)),
     include_package_data=True,
     install_requires=["anytree"],
     entry_points={
